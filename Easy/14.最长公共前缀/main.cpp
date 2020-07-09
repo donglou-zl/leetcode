59692f93c6312ac0b6357b012b8cf323e01d1f8c
donglou <donglou_zl@163.com>
2020-07-09 23:45:27 +0800

    impl easy-0, medium-0, hard-0

--- Easy/14.最长公共前缀/main.cpp
+++ Easy/14.最长公共前缀/main.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         const int64_t str_cnt = strs.size();
-        string result = (str_cnt > 0) : strs.at(0) : "";
+        string result = (str_cnt > 0) ? strs.at(0) : "";
         int prefix_len = 0;
         bool finish = false;
         for (int i = 0; i < result.length(); ++i) {
             for (int j = 1; j < strs.size(); ++j) {
-                if (strs.at(j).length() >= (i + 1) && strs.at(j)[i])
+                if (strs.at(j).length() < (i + 1)) {
+                    finish = true;
+                    break;
+                } else if (strs.at(j)[i] != result[i]) {
+                    finish = true;
+                    break;
+                }
+            }
+            if (finish) {
+                break;
+            } else {
+                prefix_len++;
             }
         }
-        
+        return result.substr(0, prefix_len);
     }
 };
